7d3a41e56346ae351a14be90bd3d77c81147b64d
omorishaoba-alt <omorishaoba@gmail.com>
2025-12-26 09:55:06 +0100

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,11 @@
+package com.fstl.oshobe;
+
+import android.os.Bundle;
+import com.google.androidbrowserhelper.trusted.TwaLauncherActivity;
+
+public class MainActivity extends TwaLauncherActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
